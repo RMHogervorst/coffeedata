4c1b475d5078eef7a0009975284848c3c9b54037
Roel Hogervorst <hogervorst.rm@gmail.com>
2016-03-07 11:08:57 +0100

    file as package, recreated the file with only coffee timestamps

--- R/savingthefiletopackage.R
+++ R/savingthefiletopackage.R
@@ -1,4 +1,4 @@
 #from raw csv file to r object
 Coffeedata<-readr::read_csv("inst/extdata/Coffeedata.csv")
 #or read.csv()
-devtools::use_data(Coffeedata)
+devtools::use_data(Coffeedata, overwrite = TRUE)
